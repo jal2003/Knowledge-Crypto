5f34a61fe4d0e3af0f33658b17b6f3bdd1a468a5
jal2003 <jaldesai715@gmail.com>
2024-07-11 23:38:06 -0400

    github

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Copright @ 2024, KnowledgeCrypto - All Rights Reserved - Made by: Jal Desai</p>
+      
+    </div>
+  )
+}
+
+export default Footer
